824f77f0a454a801f46ee22f3093c34f5df6f193
Patrick Brett <patrickbrett@users.noreply.github.com>
2021-03-14 13:12:29 +1100

    Build system for uploading photos

--- frontend/src/app/uploader/uploader.component.ts
+++ frontend/src/app/uploader/uploader.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-uploader',
   templateUrl: './uploader.component.html',
-  styleUrls: ['./uploader.component.scss']
+  styleUrls: ['./uploader.component.scss'],
 })
 export class UploaderComponent implements OnInit {
+  upload: any = {};
+  uploadUrl: any = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  async getUploadUrl() {
+    const response = (
+      await axios.put(
+        'https://pn53za3axf.execute-api.ap-southeast-2.amazonaws.com/Prod/images'
+      )
+    ).data;
+    console.log(response);
+
+    this.uploadUrl = response.uploadUrl;
   }
 
+  setFile = (e) => {
+    const { target } = e;
+    const file = target?.files && target.files[0];
+
+    if (!file) return;
+
+    var url = URL.createObjectURL(file);
+    var img = new Image();
+    img.src = url;
+
+    img.onload = () => {
+      this.upload.src = img.src;
+      this.upload.file = file;
+    };
+  };
+
+  submitImage = async (e) => {
+    e.preventDefault();
+
+    const { file } = this.upload;
+
+    try {
+      await axios.put(this.uploadUrl, file, {
+        headers: { 'Content-Type': 'image/jpeg' },
+      });
+      
+      console.log('uploaded!');
+    } catch (err) {
+      console.error(err, err.message);
+    }
+  };
 }
