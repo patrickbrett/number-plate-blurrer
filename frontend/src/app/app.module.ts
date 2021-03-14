63abae86fc0f9f9e204d1d6619443fe1ec62aab5
Patrick Brett <patrickbrett@users.noreply.github.com>
2021-03-14 11:58:45 +1100

    Create uploader component and comms service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploaderComponent } from './uploader/uploader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploaderComponent
   ],
   imports: [
     BrowserModule,
